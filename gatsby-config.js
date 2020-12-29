7a4048738811a788ec1ba68ce77b765fe6c9f798
Omar Fonseca <omar.a.fonseca@gmail.com>
2020-12-28 17:09:30 -0800

    change theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
-        theme_color: `#663399`,
+        theme_color: `#ff9913`,
         display: `minimal-ui`,
         icon: `src/images/logo.svg`, // This path is relative to the root of the site.
       },
